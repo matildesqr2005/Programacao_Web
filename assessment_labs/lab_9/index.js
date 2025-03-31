fb38ee21120b4865e877efb779d1c432deb61fc2
Alessandra Delgado <alessandra.delgado@ubi.pt>
2025-03-31 12:01:48 +0100

    lab2 setup

---
+++ assessment_labs/lab_9/index.js
@@ -0,0 +1,22 @@
+// Importar o módulo express
+const express = require('express');
+const cors = require('cors');
+
+// Criar uma instância do aplicativo Express
+const app = express();
+
+// Definir a porta onde o servidor vai escutar
+const port = 3000;
+
+// Middleware para analisar o corpo das requisições como JSON
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(204);
+})
+
+// Iniciar o servidor
+app.listen(port, () => {
+    console.log(`Servidor em http://localhost:${port}`);
+});
