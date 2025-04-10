6b9d8eb688ca6f1d8d57223b1bd253365cab3c00
MatildeS <matilde.sequeira@ubi.pt>
2025-04-10 15:07:00 +0100

    added js

--- assessment_labs/lab_9/index.js
+++ assessment_labs/lab_9/index.js
@@ -1,23 +1,39 @@
 // Importar o módulo express
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
+const path = require('path');
 // Criar uma instância do aplicativo Express
 const app = express();
 
 // Definir a porta onde o servidor vai escutar
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
 // Middleware para analisar o corpo das requisições como JSON
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.status(204);
-})
+
+app.get('/client/:id', (req, res) => {
+    const clientId = req.params.id;
+    const filePath = path.join(__dirname, 'data.json');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao ler o arquivo' });
+        }
+        const jsonData = JSON.parse(data);
+        if (jsonData.clienteId === clientId) {
+            return res.status(200).json(jsonData);
+        } else {
+            return res.status(404).json({ error: 'Cliente não encontrado' });
+        }
+    });
+});
 
 // Iniciar o servidor
 app.listen(port, () => {
     console.log(`Servidor em http://localhost:${port}`);
 });
-
 
