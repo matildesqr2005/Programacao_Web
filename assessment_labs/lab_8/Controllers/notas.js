a052b1977b5fcacda39f464465995a211eb2e411
Carolina <carolina.gegaloto@ubi.pt>
2025-04-12 16:54:16 +0100

    controllers added + first router changes

--- assessment_labs/lab_8/Controllers/notas.js
+++ assessment_labs/lab_8/Controllers/notas.js
@@ -1,51 +1,12 @@
-// Importar o módulo express
 const express = require('express');
-const cors = require('cors');
 
+const router = express.Router();
 
-
-// Criar uma instância do aplicativo Express
-const app = express();
-
-// Definir a porta onde o servidor vai escutar
-const port = 3000;
-
-// Ex 2
-let minhas_notas = [20,10,15,17];
-let notas = [
-    { 
-    cod: 0,
-    nome_disc: "Programação",
-    nome_prof: "Nuno",
-    nota: 20,
-    },
-
-    { 
-        cod: 1,
-        nome_disc: "Programação",
-        nome_prof: "Onun",
-        nota: 20,
-    },
-
-    { 
-        cod: 2,
-        nome_disc: "Programação",
-        nome_prof: "Nnou",
-        nota: 20,
-    }
-]
-
-// Middleware para analisar o corpo das requisições como JSON
-app.use(express.json());
-app.use(cors());
-
-// Ex 3 - a) V
-app.get('/', (req, res) => {
-    res.status(200).json(notas);
+router.get('/', (req, res) => {
+    res.status(200).json(minhas_notas);
 })
 
-// Ex - b) V
-app.get('/:index', (req, res) => {
+router.get('/:index', (req, res) => {
     let i = req.params.index;
     let size = minhas_notas.length;
     if(i > -1 && i < size){
@@ -57,8 +18,7 @@ app.get('/:index', (req, res) => {
     throw "Invalid index";
 })
 
-// Ex - c) V
-app.post('/', (req, res) => {
+router.post('/', (req, res) => {
     let { nota } = req.body;
     let int = parseInt(nota);
     if (isNaN(int)){
@@ -69,8 +29,7 @@ app.post('/', (req, res) => {
     res.send(minhas_notas);
 })
 
-//d)
-app.post('/:valor', (req, res) => {
+router.post('/:valor', (req, res) => {
     let valor = parseInt(req.params.valor);
     if (isNaN(valor)){
         res.status(400).json({message: 'Valor não é um inteiro.'});
@@ -80,8 +39,7 @@ app.post('/:valor', (req, res) => {
     return res.status(200).json(minhas_notas);
 })
 
-//e)
-app.patch('/:valor', (req, res) => {
+router.patch('/:valor', (req, res) => {
     let index = parseInt(req.params.valor);
     let { nota } = req.body;
     let notaInt = parseInt(nota);
@@ -93,8 +51,7 @@ app.patch('/:valor', (req, res) => {
     return res.status(400).json({message: 'Index não existente.'});
 });
 
-//f)
-app.delete('/:valor', (req, res) => {
+router.delete('/:valor', (req, res) => {
     let int = parseInt(req.params.valor);
     if( minhas_notas.length > int && int >=0 ){
         minhas_notas.splice(int,1);
@@ -103,14 +60,11 @@ app.delete('/:valor', (req, res) => {
         return res.status(400).json({message: 'Index não existente.'});
 });
 
-//g) V
-app.delete('/', (req, res) => {
+router.delete('/', (req, res) => {
     minhas_notas.length = 0;
     return res.status(200).json(minhas_notas);
 });
 
-// Iniciar o servidor
-app.listen(port, () => {
-    // Ex 1
-    console.log(`Servidor em http://localhost:${port}\nNomes: Matilde Sequeira, Alessandra Delgado, Ana Silva, Carolina Gegaloto`);
-});
+
+
+module.exports = router;
