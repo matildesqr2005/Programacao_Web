70c00001db9e56f89d9ad5d421bbcdbd7429d3f3
Alessandra Delgado <alessandra.delgado@ubi.pt>
2025-05-03 12:35:22 +0100

    Added node_2 back in

---
+++ assessment_labs/lab_node_2/public/client.js
@@ -0,0 +1,63 @@
+document.getElementById('idForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const clientId = document.getElementById('id').value;
+    fetchClientData(clientId);
+});
+
+function fetchClientData(clientId) {
+    fetch(`http://localhost:3000/client/${clientId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Client not found!!');
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayClientData(data);
+        })
+        .catch(error => {
+            alert(error.message);
+            clearDisplay();
+        });
+}
+
+function displayClientData(clientData) {
+    document.getElementById('name').value = clientData.nome;
+    document.getElementById('adrStreet').value = clientData.endereco.rua;
+    document.getElementById('adrNo').value = clientData.endereco.numero;
+    document.getElementById('adrCity').value = clientData.endereco.cidade;
+    document.getElementById('adrPostal').value = clientData.endereco.codigoPostal;
+    document.getElementById('adtResid').value = clientData.informacoesAdicionais.tipoTarifa;
+    document.getElementById('adrProv').value = clientData.informacoesAdicionais.fornecedorEnergia;
+    if (clientData.informacoesAdicionais.contratoAtivo) {
+        document.getElementById('adtActive').value = 'Sim';
+    } else {
+        document.getElementById('adtActive').value = 'Não';
+    }
+
+    const consumoBody = document.getElementById('consumBody');
+    consumoBody.innerHTML = ''; 
+    clientData.consumo.forEach(item => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${item.mes}</td>
+            <td>${item.ano}</td>
+            <td>${item.kWhConsumido}</td>
+            <td>${item.custoTotal.toFixed(2)}</td>
+            <td>${item.dataLeitura}</td>
+        `;
+        consumoBody.appendChild(row);
+    });
+}
+
+function clearDisplay() {
+    document.getElementById('name').value = '';
+    document.getElementById('adrStreet').value = '';
+    document.getElementById('adrNo').value = '';
+    document.getElementById('adrCity').value = '';
+    document.getElementById('adrPostal').value = '';
+    document.getElementById('adtResid').value = '';
+    document.getElementById('adrProv').value = '';
+    document.getElementById('adtActive').value = '';
+    document.getElementById('consumBody').innerHTML = '';
+}
